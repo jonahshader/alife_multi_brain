f6c8f7666e7a119e5dfd7b7912a3581a97c92756
Jonah Shader <jonah.shader@gmail.com>
2021-11-25 13:16:52 -0500

    doin stuff. i forgot

--- core/src/main/java/com/jonahshader/systems/brain/visualizer/NeuronGraphic.kt
+++ core/src/main/java/com/jonahshader/systems/brain/visualizer/NeuronGraphic.kt
@@ -2,16 +2,29 @@ package com.jonahshader.systems.brain.visualizer
 
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.math.Vector2
 import com.jonahshader.MultiBrain
 import com.jonahshader.systems.scenegraph.Node2D
+import ktx.math.plusAssign
 
 abstract class NeuronGraphic : Node2D() {
-    val weights = mutableListOf<WeightGraphic>()
+    val weights = mutableListOf<WeightGraphic>() // for rendering ??? might not need
     val color = Color(1f, 1f, 1f, 1f)
     var radius = 32f
+    var velocity = Vector2()
+    var acceleration = Vector2()
+    var force = Vector2()
+    var mass = 1.0f
 
     override fun customUpdate(dt: Float) {
-        TODO("Update weight spring physics. also slow down ")
+        force.scl(1/mass)
+        acceleration.set(force)
+        acceleration.scl(dt)
+        velocity += acceleration
+        localPosition.add(velocity.x * dt, velocity.y * dt)
+
+        force.setZero()
+
     }
 
     override fun customRender(batch: Batch) {
