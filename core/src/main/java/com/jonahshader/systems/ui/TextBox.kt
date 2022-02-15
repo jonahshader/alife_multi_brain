329ceb348102a673773dd3b6621c16d46e610952
Jonah Shader <jonah.shader@gmail.com>
2022-02-15 01:08:04 -0500

    added note

--- core/src/main/java/com/jonahshader/systems/ui/TextBox.kt
+++ core/src/main/java/com/jonahshader/systems/ui/TextBox.kt
@@ -6,4 +6,8 @@ class TextBox(pos: Vector2, size: Vector2, minSize: Vector2) : Window(pos, size,
     //TODO: text box may or many not be manually resizable. that functionality should
     //  probably be put in Window and have it configurable. it will be movable though
     //  it should auto calculate the size based on font size, line distance, and padding
+
+    //TODO but not for this: i need a callback function thingy for simulations to get
+    //  periodic updates on fitness
+    // this is necessary because threads n stuff
 }
