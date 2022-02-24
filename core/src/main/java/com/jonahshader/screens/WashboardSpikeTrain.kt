b3e0a3f617e28ee8899bedba874532c3ca96671e
jonah <jonah.shader@gmail.com>
2022-02-24 10:26:38 -0500

    added more plots

--- core/src/main/java/com/jonahshader/screens/WashboardSpikeTrain.kt
+++ core/src/main/java/com/jonahshader/screens/WashboardSpikeTrain.kt
@@ -12,11 +12,13 @@ import com.jonahshader.systems.ui.ScreenWindow
 import com.jonahshader.systems.ui.Slider
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
+import kotlin.math.PI
 
 class WashboardSpikeTrain : KtxScreen {
     private val window = ScreenWindow(Vector2(1280f, 720f))
-    private val voltagePlot = Plot("Time (unit)", "Output Voltage", "Washboard Voltage", Vector2(), Vector2(1280f, 320f))
-    private val // TODO: phase, current
+    private val voltagePlot = Plot("Time (unit)", "Output Voltage", "Washboard Voltage", Vector2(), Vector2(1280f, PLOT_HEIGHT))
+    private val phasePlot = Plot("Time (unit)", "Phase Degrees", "Washboard Phase", Vector2(0f, PLOT_HEIGHT), Vector2(1280f, PLOT_HEIGHT))
+    private val currentPlot = Plot("Time (unit)", "Current", "Washboard Current", Vector2(0f, PLOT_HEIGHT * 2), Vector2(1280f, PLOT_HEIGHT))
     private val neurons = mutableListOf<WashboardNeuron>()
 
 //    private var bias = 198 * 10e-6f
@@ -27,14 +29,20 @@ class WashboardSpikeTrain : KtxScreen {
     companion object {
         private const val SIM_STEPS = 400
         private const val NEURON_COUNT = 8
+        private const val PLOT_HEIGHT = 200f
     }
 
     init {
-        window.addChildWindow(voltagePlot)
-        voltagePlot.addTrend(Plot.Trend("Input Current", Color.BLUE, true, Plot.Mode.LINE))
+        window += voltagePlot
+        window += phasePlot
+        window += currentPlot
+
+        currentPlot.addTrend(Plot.Trend("Input Current", Color.BLUE, true, Plot.Mode.LINE))
+
 
         (0 until NEURON_COUNT).forEachIndexed{ index, _ ->
             voltagePlot.addTrend(Plot.Trend("Voltage $index", Color(1f, 1f, 1f, 1f).fromHsv(index.toFloat() * 360f / NEURON_COUNT, 1f, 1f), true, Plot.Mode.LINE))
+            phasePlot.addTrend(Plot.Trend("Phase $index", Color(1f, 1f, 1f, 1f).fromHsv(index.toFloat() * 360f / NEURON_COUNT, 1f, 1f), true, Plot.Mode.LINE))
         }
 
         window += Slider("Bias", 0f, 900e-6f, bias, Vector2(0f, 680f), size = Vector2(1280f, 40f)) { bias = it; generate() }
@@ -48,7 +56,10 @@ class WashboardSpikeTrain : KtxScreen {
     private fun generate() {
         neurons.clear()
         voltagePlot.clearData()
-        for (i in 0 until 15) {
+        phasePlot.clearData()
+        currentPlot.clearData()
+
+        for (i in 0 until NEURON_COUNT) {
             neurons += WashboardNeuron(a)
 //            neurons.last().bias = (198 * 10e-6).toFloat()
             neurons.last().bias = bias
@@ -65,7 +76,7 @@ class WashboardSpikeTrain : KtxScreen {
 //            var inputCurrent = 0.0065f// 0.0065f
 //            if (i > 12) inputCurrent = 0f
 
-            voltagePlot.addDatum("Input Current", Vector2(i.toFloat(), inputCurrent))
+            currentPlot.addDatum("Input Current", Vector2(i.toFloat(), inputCurrent))
             neurons[0].addWeightedOutput(inputCurrent)
             neurons[0].update(1/1000f)
             neurons[0].updateOutput()
@@ -77,6 +88,7 @@ class WashboardSpikeTrain : KtxScreen {
 
             neurons.forEachIndexed { index, it ->
                 voltagePlot.addDatum("Voltage $index", Vector2(i.toFloat(), it.out))
+                phasePlot.addDatum("Phase $index", Vector2(i.toFloat(), (it.angleD * 360 / (2 * PI)).toFloat()))
             }
         }
     }
